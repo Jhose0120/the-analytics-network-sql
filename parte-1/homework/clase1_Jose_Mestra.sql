caf682ad83c3237589ca3531577e7969952c6e13
Jhose0120 <109871075+Jhose0120@users.noreply.github.com>
2023-03-24 13:33:03 -0500

    Update clase1_Jose_Mestra.sql
    Ajuste de punto

--- parte-1/homework/clase1_Jose_Mestra.sql
+++ parte-1/homework/clase1_Jose_Mestra.sql
@@ -11,7 +11,7 @@ SELECT * FROM stg.store_master WHERE pais = 'Argentina' ORDER BY fecha_apertura
 --6. Cuales fueron las ultimas 5 ordenes de ventas?
 SELECT * FROM stg.order_line_sale ORDER BY fecha DESC limit 5
 --7. Mostrar los primeros 10 registros de el conteo de trafico por Super store ordenados por fecha.
-SELECT * FROM stg.super_store_count ORDER BY fecha LIMIT 10
+SELECT tienda, cast(fecha as date), conteo FROM stg.super_store_count ORDER BY fecha LIMIT 10
 --8. Cuales son los producto de electro que no son Soporte de TV ni control remoto.
 SELECT * FROM stg.product_master WHERE categoria = 'Electro' AND subsubcategoria != 'Soporte' AND subsubcategoria != 'Control remoto'
 --9. Mostrar todas las lineas de venta donde el monto sea mayor a $100.000 solo para transacciones en pesos.
