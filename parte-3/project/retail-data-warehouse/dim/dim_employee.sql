244ee550e5673213404d2dedfb509a0e719cbfcb
Jhose0120 <109871075+Jhose0120@users.noreply.github.com>
2023-05-18 17:22:34 -0500

    Create dim_employee.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_employee.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS stg.employee;
+
+CREATE TABLE IF NOT EXISTS stg.employee
+(
+    ,id_employee integer PRIMARY KEY
+    ,nombre character varying(100) 
+    ,apellido character varying(100) 
+    ,fecha_entrada date NOT NULL
+    ,fecha_salida date
+    ,telefono character varying(100) 
+    ,pais character varying(225) 
+    ,provincia character varying(225) 
+    ,codigo_tienda character varying(225) 
+    ,posicion character varying(225)
+    ,is_active boolean
+)
